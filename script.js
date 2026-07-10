cc58ffc4bf772d77cb6c2692cb0dbb25af6e0540
Ishola Habeeb <isholahbaeeb1@outlook.com>
2026-07-10 07:22:11 -0400

    Added new portfolio updates

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const navItems = document.querySelectorAll('.nav p');
+//Theme
+function theme() {
+  const lightBtn = document.getElementById('lightBtn');
+  const darkBtn = document.getElementById('darkBtn');
+  const body = document.body;
+
+  body.classList.toggle('light-mode');
+
+  if (body.classList.contains('light-mode')) {
+    lightBtn.style.display = 'none';
+    darkBtn.style.display = 'inline-block';
+  } else {
+    lightBtn.style.display = 'inline-block';
+    darkBtn.style.display = 'none';
+  }
+}
+  
+  const navItems = document.querySelectorAll('.nav p');
 
 navItems.forEach(item => {
   item.addEventListener('click', () => {
@@ -10,5 +27,5 @@ navItems.forEach(item => {
 
 const sayHelloBtn = document.querySelector('.sayhello-btn');
 sayHelloBtn.addEventListener('click', () => {
-  window.location.href = 'mailto:isholahabeeb1@outlook.com';
+  window.location.href = 'mailto:hoishola973@gmail.com';
 });
